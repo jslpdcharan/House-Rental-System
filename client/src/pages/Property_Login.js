8327737dedd4c64d2db9245fd093ea873c1a42c2
Sasidhar Dimmiti <sasidhardimmiti2000@gmail.com>
2024-12-01 10:43:39 -0600

    Added Property_Admin.js Property_Login.js Request_Status.js Requests_Form.js files

---
+++ client/src/pages/Property_Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from "../components/NavBar";
+import Property_Admin from "../components/Property_Admin";
+
+export default function Property_Login(){
+    return (
+        <>
+            <NavBar />
+            <Property_Admin />
+        </>
+    )
+}
---
+++ client/src/pages/Request_Status.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from "../components/NavBar";
+import Request_Form from "../components/Requests_Form";
+
+export default function Request_Status(){
+    return (
+        <>
+            <NavBar />
+            <Request_Form />
+        </>
+    )
+}
