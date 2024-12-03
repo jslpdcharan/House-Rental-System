447cdf4464ebb87477f937d32394c203504a4d86
Likitha-Paruchuri <likitha.paruchuri@okstate.edu>
2024-12-03 15:32:09 -0600

    Add files via upload

---
+++ client/src/pages/Add_Property.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from "../components/NavBar";
+import Property_Form from "../components/Property_Form";
+
+export default function Add_Property(){
+    return (
+        <>
+            <NavBar />
+            <Property_Form />
+        </>
+    )
+}
