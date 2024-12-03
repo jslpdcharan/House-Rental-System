13983c0df4b4f96388b7039eef6f49ae9dcc6097
Likitha-Paruchuri <likitha.paruchuri@okstate.edu>
2024-12-03 15:05:19 -0600

    Add files via upload

---
+++ client/src/pages/Home_Page.js
@@ -0,0 +1,11 @@
+import NavBar from "../components/NavBar";
+import React from 'react';
+import Property_View from "../components/Property_View";
+export default function Home_Page(){
+    return (
+        <>
+            <NavBar />
+            <Property_View />
+        </>
+    )
+}
